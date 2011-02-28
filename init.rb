c182c7cf4e8d71d56799a1c5ba9dc4543a242043
Alex Shulgin <ash@commandprompt.com>
2011-02-28 21:58:54 +0200

    Added license.

--- init.rb
+++ init.rb
@@ -5,8 +5,10 @@ require 'lock_users_helper_patch'
 Redmine::Plugin.register :redmine_spam_lock do
   name 'Redmine Spam Lock plugin'
   author 'Alex Shulgin <ash@commandprompt.com>'
-  description 'A plugin to allow lock unregistered users w/o the need to activate them first'
+  description 'A plugin to allow locking unregistered users without the need to activate them first.'
   version '0.0.1'
-  #url 'http://example.com/path/to/plugin'
+  url 'http://github.com/a1exsh/redmine_spam_lock'
   #author_url 'http://example.com/about'
+
+  requires_redmine :version_or_higher => '1.1.0'
 end
--- lib/lock_users_helper_patch.rb
+++ lib/lock_users_helper_patch.rb
@@ -1,3 +1,22 @@
+# redmine_spam_lock -- A Redmine plugin to allow locking unregistered users
+# without the need to activate them first.
+#
+# Copyright (C) 2011  Alex Shuglin <ash@commandprompt.com>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 require_dependency 'users_helper'
 
 module LockUsersHelperPatch
@@ -20,6 +39,9 @@ module LockUsersHelperPatch
     end
 
     def lock_user_link(user)
+      # NB: for redmine-0.9.x you might want :action => 'edit' and
+      # :method => :post below
+
       url = {:controller => 'users', :action => 'update', :id => user, :page => params[:page], :status => params[:status], :tab => nil}
       link_to l(:button_lock), url.merge(:user => {:status => User::STATUS_LOCKED}), :method => :put, :class => 'icon icon-lock'
     end
