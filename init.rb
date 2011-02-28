86875eb9b3d680c82f29fa0b5bdbf786dddfb222
Alex Shulgin <ash@commandprompt.com>
2011-02-28 07:16:13 -0800

    Initial commit.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_spam_lock do
+  name 'Redmine Spam Lock plugin'
+  author 'Alex Shulgin <ash@commandprompt.com>'
+  description 'A plugin to allow lock unregistered users w/o the need to activate them first'
+  version '0.0.1'
+  #url 'http://example.com/path/to/plugin'
+  #author_url 'http://example.com/about'
+end
